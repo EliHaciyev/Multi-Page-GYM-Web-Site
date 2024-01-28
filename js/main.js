69a0c0d83dad01be4cac3c1b8d95e0e999f87ba9
Ali Haciyev <heli25851@gmail.com>
2024-01-28 23:16:51 +0400

    The second part of the job is done

--- js/main.js
+++ js/main.js
@@ -7,11 +7,21 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times")
   navbar.classList.toggle('active')
 }
-
-
-
-
-
-
-
 /*  menu script end */
+
+/* light gallery script start */
+
+lightGallery(document.querySelector(".gallery"))
+
+/* light gallery script end */
+
+/* loader start */
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("active")
+}
+function fadeOut() {
+  setTimeout(loader, 1500)
+}
+fadeOut()
+/* loader end */
